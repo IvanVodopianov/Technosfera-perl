9df46e8207e009d25b1d1c487c53c46f0394c0a6
Ivan Vodopianov <vodopianov@ya.ru>
2017-04-24 21:27:57 +0300

    log-analyzer regex written

--- homeworks/log-analyzer/bin/analyze.pl
+++ homeworks/log-analyzer/bin/analyze.pl
@@ -2,6 +2,7 @@
 
 use strict;
 use warnings;
+use DDP;
 our $VERSION = 1.0;
 
 
@@ -14,20 +15,43 @@ report($parsed_data);
 exit;
 
 sub parse_file {
+    my $line_rgx = qr/^ (?<ip_address>\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\s+
+		                  \[(?<time_stamp>.*)\]\s+
+											\"(?<request>.*)\"\s+
+											  (?<code>\d{3})\s+
+												(?<bytes>\d*)\s+
+												\"(?<referrer>.*)\"\s+
+											\"(?<user_agent>.*)\"\s+
+											\"(?<compress>.*)\"
+											$/x;
+		my $time_stamp_rgx = qr/ (?<day>\d{2})\/
+		                   (?<month>\w{3})\/
+											 (?<year>\d{4})
+											:(?<hour>\d{2})
+											:(?<min>\d{2})
+											:(?<sec>\d{2})\s{1}
+											 (?<time_zone>\+\d{4})
+											$/x;
     my $file = shift;
 
     # you can put your code here
-
+		my $href = {};
     my $result;
     open my $fd, "-|", "bunzip2 < $file" or die "Can't open '$file': $!";
+		my $i = 1;
     while (my $log_line = <$fd>) {
 
         # you can put your code here
         # $log_line contains line from log file
+$log_line =~ $line_rgx;
+    unless (exists $href->{$1}) {$href->{$1} = [];}
+		push @{$href->{$1}}, $2;
+
+    if ($i++ > 15) {last;}
 
     }
     close $fd;
-
+		p $href;
     # you can put your code here
 
     return $result;
