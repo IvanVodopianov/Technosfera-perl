ce31bd2afc5160b55c92f26839e49abb301466b1
Ivan Vodopianov <vodopianov@ya.ru>
2017-04-25 14:05:39 +0300

    log-analyzer: parsing done

--- homeworks/log-analyzer/bin/analyze.pl
+++ homeworks/log-analyzer/bin/analyze.pl
@@ -1,5 +1,6 @@
 #!/usr/bin/perl
 
+use 5.010;
 use strict;
 use warnings;
 use DDP;
@@ -14,51 +15,105 @@ my $parsed_data = parse_file($filepath);
 report($parsed_data);
 exit;
 
+#sub moving_average {
+#    my $list = shift;
+#    my $period = shift;
+#    my $tmp = [];
+#    foreach my $el @$list {
+
+#    }
+#}
+
 sub parse_file {
-    my $line_rgx = qr/^ (?<ip_address>\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\s+
-		                  \[(?<time_stamp>.*)\]\s+
-											\"(?<request>.*)\"\s+
-											  (?<code>\d{3})\s+
-												(?<bytes>\d*)\s+
-												\"(?<referrer>.*)\"\s+
-											\"(?<user_agent>.*)\"\s+
-											\"(?<compress>.*)\"
-											$/x;
-		my $time_stamp_rgx = qr/ (?<day>\d{2})\/
-		                   (?<month>\w{3})\/
-											 (?<year>\d{4})
-											:(?<hour>\d{2})
-											:(?<min>\d{2})
-											:(?<sec>\d{2})\s{1}
-											 (?<time_zone>\+\d{4})
-											$/x;
+    my $line_regex = qr/^
+      (?<ip_address>\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\s+
+    \[(?<time_stamp>.*)\]\s+
+    \"(?<request>.*)\"\s+
+      (?<code>\d{3})\s+
+      (?<bytes>\d*)\s+
+    \"(?<referrer>.*)\"\s+
+    \"(?<user_agent>.*)\"\s+
+    \"(?<compress>.*)\"
+    $/x;
+    my $time_stamp_regex = qr/
+     (?<day>\d{2})\/
+     (?<month>\w{3})\/
+     (?<year>\d{4})
+    :(?<hour>\d{2})
+    :(?<min>\d{2})
+    :(?<sec>\d{2})\s{1}
+     (?<time_zone>\+\d{4})
+    $/x;
+
     my $file = shift;
 
     # you can put your code here
-		my $href = {};
-    my $result;
+
+    my $href_time = {};
+    my $href_status = {};
+
+    my $result = [];
+
     open my $fd, "-|", "bunzip2 < $file" or die "Can't open '$file': $!";
-		my $i = 1;
+    my $i = 1;
+
     while (my $log_line = <$fd>) {
 
         # you can put your code here
         # $log_line contains line from log file
-$log_line =~ $line_rgx;
-    unless (exists $href->{$1}) {$href->{$1} = [];}
-		push @{$href->{$1}}, $2;
+
+    $log_line =~ $line_regex;
+
+    my $ip_address = $1;
+    my $time_stamp = $2;
+    my $code = $4;
+    my $bytes = $5;
+    my $compress_ratio = $8;
+
+    unless (exists $href_time->{$ip_address}) {$href_time->{$ip_address} = {};}
+
+    unless (exists $href_status->{$ip_address}) {$href_status->{$ip_address} = {};}
+
+    $time_stamp =~ $time_stamp_regex;
+    my $h = $4;
+    my $m = $5;
+    my $s = $6;
+
+    unless (exists $href_time->{$ip_address}->{60*$h+$m}) {$href_time->{$ip_address}->{60*$h+$m} = 1;}
+
+    $href_time->{$ip_address}->{60*$h+$m} += 1;
+
+    unless (exists $href_status->{$ip_address}->{$code}) {$href_status->{$ip_address}->{$code} = [];}
+
+    my $tmp = [];
+    push @{$tmp}, $bytes, $compress_ratio;
+    push @{$href_status->{$ip_address}->{$code}}, $tmp;
+#        push @{$href->{$ip_address}}, $h*3600+$m*60+$s;
 
     if ($i++ > 15) {last;}
 
     }
     close $fd;
-		p $href;
+#                p $href_time;
+                p $href_status;
     # you can put your code here
+    push @{$result}, $href_time, $href_status;
 
     return $result;
 }
 
 sub report {
     my $result = shift;
+    say "from report:";
+    say ref @{$result}[0];
+    say ref @{$result}[1];
+    my $href_time = @{$result}[0];
+    my $href_time_count_avg = {};
+    p $href_time;
+    foreach (keys %$href_time) {
+    unless (exists $href_time_count_avg->{$_}) {exists $href_time_count_avg->{$_} = {};}
+    foreach (values)
+    }
 
     # you can put your code here
 
