ab2ddf3c2cc8a633c67367a6520c06c74c48b770
Ivan Vodopianov <vodopianov@ya.ru>
2017-05-01 23:37:39 +0300

    myconst: started

--- homeworks/myconst/lib/myconst.pm
+++ homeworks/myconst/lib/myconst.pm
@@ -2,7 +2,38 @@ package myconst;
 
 use strict;
 use warnings;
-use Scalar::Util 'looks_like_number';
+use 5.010;
+#use Scalar::Util 'looks_like_number';
+use DDP;
+#use Data::Dumper;
+
+#use Exporter 'import';
+our $params;
+#say @_;
+#say Dumper \%params;
+#p \%params;
+our @EXPORT = qw(m2 m3 pi zero);
+our %EXPORT_TAGS = (
+all => [qw(m1 m2 m3)],
+math => [qw(m1 m2)],
+phys => [qw(m3)],
+);
+our $zero = 0;
+our $pi = 3.13;
+sub m1 { shift() ** 1 }
+sub m2 { shift() ** 2 }
+sub m3 { shift() ** 3 }
+#sub zero { shift() }
+#sub pi { shift() }
+sub import {
+p @_;
+}
+#my ($package, %params) = @_;
+#$params = $msg;
+#p %params;
+#say $package;
+#say $msg;
+#say @_;
 
 
 =encoding utf8
